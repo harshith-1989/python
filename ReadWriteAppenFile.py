79cfbc0007655cf0dc2fed977f39a686d86294ea
harshith <pdharsh@gmail.com>
2017-11-07 22:36:34 +0530

    genrators

--- ReadWriteAppenFile.py
+++ ReadWriteAppenFile.py
@@ -3,10 +3,10 @@ def return_filehandle_for_reading (fileName):
     return open(fileName,'r',-1,"utf-8")
 
 def return_filehandle_for_writing (fileName):
-    return open(fileName,'w')
+    return open(fileName,'w',-1,"utf-8")
 
 def return_filehandle_for_read_and_write (fileName):
-    return open(fileName,'w+')
+    return open(fileName,'w+',-1,"utf-8")
 
 fopen = return_filehandle_for_reading("/Users/harshith/PycharmProjects/python/regex_patterns.txt")
 print (fopen.readlines())
